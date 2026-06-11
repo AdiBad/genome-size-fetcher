dc3bdf00cfb89a73e5c5930738202fad3ff4f99d
notesbyadi@gmail.com <notesbyadi@gmail.com>
2026-06-11 22:20:47 +0000

    Created accessory_tools and bac-gen-logo.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,31 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
+    
+    #genomes_list = anvil.server.call('fetch_genome_data')
+    #self.drop_down_1.items = [row['species'] for row in genomes_list]
 
-    # Any code you write here will run before the form opens.
+    genomes_list = app_tables.genome_sizes.search()
+    self.drop_down_1.items = [row['species'] for row in genomes_list]
+    
+
+  @handle("button_1", "click")
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    user_choice = self.drop_down_1.selected_value
+    
+    #detected_size = anvil.server.call('fetch_genome_size', user_choice)
+    #self.text_box_1.text = detected_size['genome_size_mbp']
+
+    genome_size = app_tables.genome_sizes.get(species=user_choice)
+    self.text_box_1.text = genome_size['genome_size_mbp']
+    
+    
---
+++ server_code/accessory_tools.py
@@ -0,0 +1,26 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+import pandas as pd
+
+@anvil.server.callable
+def fetch_genome_data()->list:
+  genome_table = pd.read_csv(data_files['genome_sizes.csv'])
+  return genome_table.to_dict('records')
+
+
+@anvil.server.callable
+def fetch_genome_size(species: str)->list:
+  genome_table = pd.read_csv(data_files['genome_sizes.csv'], 
+                             index_col=0)
+  return genome_table.loc[species].to_dict()
